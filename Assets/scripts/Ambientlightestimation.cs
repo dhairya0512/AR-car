057b9c19f0e0776a55f092d2a187c0efbcb3bcc2
Dhairya Pandya <dhairyapandya05@gmial.com>
2023-03-26 22:03:23 +0530

    itt

---
+++ Assets/scripts/Ambientlightestimation.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.UI;
+
+public class Ambientlightestimation : MonoBehaviour
+{
+    public ARCameraManager cameraManager;
+    private Light lightcomponent;
+    public Text warningtext;
+    // Start is called before the first frame update
+    private void OnEnable()
+    {
+        lightcomponent = GetComponent<Light>();
+        cameraManager.frameReceived += onCameraframereceived;
+    }
+    void onCameraframereceived(ARCameraFrameEventArgs aRCameraFrameEvent) {
+        ARLightEstimationData led = aRCameraFrameEvent.lightEstimation;
+
+        if (led.averageBrightness.HasValue)
+        {
+            lightcomponent.intensity = led.averageBrightness.Value;
+            if (led.averageBrightness.Value < 0.1f)
+            {
+                //show the warning
+                warningtext.gameObject.SetActive(true);
+            }
+            else
+            {
+                //disable the warning
+                warningtext.gameObject.SetActive(false);
+
+            }
+
+        }
+
+        if (led.averageColorTemperature.HasValue)
+        {
+            lightcomponent.colorTemperature = led.averageColorTemperature.Value;
+        }
+    
+    }
+
+    private void OnDisable()
+    {
+        cameraManager.frameReceived -= onCameraframereceived;
+
+    }
+}
---
+++ Assets/scripts/uicontroller.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class uicontroller : MonoBehaviour
+{
+    public Material carbodymaterial;
+    public GameObject colors3dui;
+    bool uistatus = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void redcolor()
+    {
+        carbodymaterial.color = new Color(0.4622642f, 0.000000f, 0.000000f, 1);
+        Debug.Log("Red btn pressed");
+    }
+
+    public void bluecolor()
+    {
+        Debug.Log("Blue btn pressed");
+
+        carbodymaterial.color = new Color(0.009504453f, 0.000000f, 0.4627451f, 1);
+    }
+
+    public void whitecolor()
+    {
+        Debug.Log("White btn pressed");
+
+        carbodymaterial.color = new Color(0.6981132f, 0.6981132f, 0.6981132f, 1);
+    }
+
+    public void Toggleui()
+    {
+        if (uistatus == true)
+        {
+            colors3dui.SetActive(false);
+            uistatus = false;
+        }
+
+        if (uistatus == false)
+        {
+            colors3dui.SetActive(true);
+            uistatus = true;
+        }
+    }
+}
